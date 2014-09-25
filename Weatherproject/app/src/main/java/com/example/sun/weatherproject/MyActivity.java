fe65bac32a92ef56004800f03ff44d5f4858c55b
Buratinkalol <h12vbn6@gmail.com>
2014-09-26 03:41:24 +0400

    base

---
+++ Weatherproject/app/src/main/java/com/example/sun/weatherproject/MyActivity.java
@@ -0,0 +1,55 @@
+package com.example.sun.weatherproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import android.os.Handler;
+import java.util.logging.LogRecord;
+
+
+public class MyActivity extends Activity {
+    private Handler h;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MyActivity.this,MyWeather.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Weatherproject/app/src/main/java/com/example/sun/weatherproject/MyWeather.java
@@ -0,0 +1,15 @@
+package com.example.sun.weatherproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sun on 26.09.2014.
+ */
+public class MyWeather extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather);
+    }
+}
